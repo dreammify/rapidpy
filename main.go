6aaf0200d46a7bc262082eae06d541ca3a1d8731
Panagiotis Lantavos-Stratigakis <pstratig@flexport.com>
2024-11-07 21:11:30 +0100

    Add main to app index

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
 
+func main() {
+	url := "https://raw.githubusercontent.com/dreammify/rapidpy/refs/heads/main/applications/index.txt"
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error fetching the URL:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading the response body:", err)
+		return
+	}
+
+	fmt.Println(string(body))
 }
