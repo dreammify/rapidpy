ba8f8ed12d6f8956ed64e816456a4e25e0f2d863
Panagiotis Lantavos-Stratigakis <35073940+dreammify@users.noreply.github.com>
2024-11-08 01:12:09 +0200

    Create cat3.py

---
+++ applications/cat3.py
@@ -0,0 +1,21 @@
+# 0
+import requests
+import flask
+from io import BytesIO
+
+if __name__ == '__main__':
+    # Url for getting a random cat picture
+    cataas_url = 'https://cataas.com/cat'
+    # Fetch random cat picture
+    cat_picture = requests.get(cataas_url).content
+
+    # Create a Flask app
+    app = flask.Flask(__name__)
+
+    # Serve the random cat picture
+    @app.route('/')
+    def home():
+        return flask.send_file(BytesIO(cat_picture), mimetype='image/jpeg')
+
+    # Run the app
+    app.run(host='0.0.0.0', port=45052)
